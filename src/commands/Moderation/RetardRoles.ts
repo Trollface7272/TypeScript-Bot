dbbcb31f2792ac8b897deba5153f437bb7175a0b
Trollface7272 <trollface7272@post.cz>
2021-08-22 06:12:38 +0200

    Added retard and retardroles commands

---
+++ src/commands/Moderation/RetardRoles.ts
@@ -0,0 +1,79 @@
+import { channel } from "diagnostics_channel";
+import { Message, Role } from "discord.js";
+import { Bot } from "../../client/Client";
+import { AddRetardRole } from "../../database/Guilds";
+import { RunFunction } from "../../interfaces/Command";
+
+const ErrorCodes = {
+    "1": "Invalid syntax",
+    "2": "Invalid role provided",
+    "3": "Insufficient permissions",
+}
+
+export const run: RunFunction = async (client: Bot, message: Message, args: string[]) => {
+    if (args[0] == "list") return List(client, message)
+    if (args[0] == "add") return Add(client, message, args)
+    if (args[0] == "remove") return Remove(client, message, args)
+    if (args[0] == "clear") return Clear(client, message, args)
+}
+
+const List = async (client: Bot, message: Message) => {
+    let roles = await client.database.Guilds.GetRetardRoles(client, message)
+    let out = ""
+    for (let i = 0; i < roles?.length; i++) {
+        let role = message.guild.roles.cache.get(roles[i]) || await message.guild.roles.fetch(roles[i])
+        out += `${i + 1}. <@&${role.id}>\n`
+    }
+    message.channel.send({ embeds: [client.embed({ description: out, title: "List of retard roles:" }, message)], allowedMentions: {roles: []} })
+}
+
+const Add = async (client: Bot, message: Message, args: string[]) => {
+    if (!message.member.permissions.has("ADMINISTRATOR")) return HandleError(client, message, 3)
+    let position = -1
+    let offset = args[1] == "before" ? -1 : args[1] == "after" ? 0 : Infinity
+    if (offset != Infinity) {
+        if (isNaN(parseInt(args[2]))) { return HandleError(client, message, 1) }
+        position = parseInt(args[2]) + offset
+    }
+    let role: Role
+    if (message.mentions.roles.size > 0) {
+        role = message.mentions.roles.first()
+    } else {
+        let id = offset == Infinity ? args[1] : args[3]
+        if (!id) return HandleError(client, message, 1)
+        role = message.guild.roles.cache.get(id) || await message.guild.roles.fetch(id)
+    }
+    if (!role) return HandleError(client, message, 2)
+    AddRetardRole(client, message, role.id, position)
+    message.channel.send({embeds: [client.embed({description: `Successfully added <@&${role.id}> to retard roles list` }, message)], allowedMentions: {roles: []}})
+    client.logger.debug({position, offset, id:role.id,})
+}
+
+const Clear = async (client: Bot, message: Message, args: string[]) => {
+    if (!message.member.permissions.has("ADMINISTRATOR")) return HandleError(client, message, 3)
+    client.database.Guilds.ClearRetardRoles(client, message)
+    message.channel.send({embeds: [client.embed({description: `Successfully cleared retard roles list` }, message)], allowedMentions: {roles: []}})
+    client.logger.debug(`${message.author.username} cleared retard roles on ${message.guild.name}`)
+}
+
+const Remove = async (client: Bot, message: Message, args: string[]) => {
+    if (!message.member.permissions.has("ADMINISTRATOR")) return HandleError(client, message, 3)
+    let id: string
+    if (message.mentions.roles.size > 0) id = message.mentions.roles.first().id
+    else if (args[1] == "id") id = args[2]
+    else if (isNaN(parseInt(args[1]))) return HandleError(client, message, 1)
+    else {
+        id = await client.database.Guilds.RemoveRetardRoleIndex(client, message, parseInt(args[1]))
+        message.channel.send({embeds: [client.embed({description: `Successfully removed <@&${id}> from retard role list.` }, message)], allowedMentions: {roles: []}})
+        return
+    }
+    client.database.Guilds.RemoveRetardRoleId(client, message, id)
+    message.channel.send({embeds: [client.embed({description: `Successfully removed <@&${id}> from retard role list.` }, message)], allowedMentions: {roles: []}})
+}
+
+const HandleError = (client: Bot, message: Message, error: any) => {
+    client.logger.debug(ErrorCodes[error] ? ErrorCodes[error] : error)
+    message.channel.send({embeds: [client.embed({description: ErrorCodes[error]}, message)]})
+}
+
+export const name: string = "retardroles"
--- src/database/Guilds.ts
+++ src/database/Guilds.ts
@@ -1,4 +1,4 @@
-import { model, Schema } from "mongoose"
+import { model, Schema, UpdateQuery } from "mongoose"
 import { Message } from 'discord.js'
 import { Bot } from "../client/Client"
 
@@ -10,12 +10,13 @@ export interface Filter {
 }
 
 export interface Guild {
-    id: String,
-    name: String,
+    id: string,
+    name: string,
     messages: number,
     commands: number,
     skeetkey_uses: number,
     prefix: string,
+    retard_roles: Array<string>,
     filter: Array<Filter>,
 }
 
@@ -26,6 +27,7 @@ const schema = new Schema<Guild>({
     commands: { type: Number, required: true },
     skeetkey_uses: { type: Number, required: true },
     prefix: { type: String, required: true },
+    retard_roles: {type: Array, required: true},
     filter: { type: Array, required: true }
 })
 
@@ -40,43 +42,67 @@ async function CreateGuild(message: Message): Promise<Guild> {
         commands: 0,
         skeetkey_uses: 0,
         prefix: "!",
+        retard_roles: [],
         filter: []
     })
     await doc.save()
     return doc
 }
 
-export const OnMessage = async (client: Bot, message: Message) => {
+export const OnMessage = async (client: Bot, message: Message): Promise<void> => {
     const res = await client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $inc: { messages: 1 } })
     if (res.matchedCount < 1) await CreateGuild(message)
 }
 
-export const OnCommand = async (client: Bot, message: Message) => {
-    const res = await client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $inc: { commands: 1 } })
-    if (res.matchedCount < 1) await CreateGuild(message)
+export const OnCommand = async (client: Bot, message: Message): Promise<void> => {
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $inc: { commands: 1 } })
 }
 
-export const SkeetkeyUsed = async (client: Bot, message: Message) => {
-    const res = await client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $inc: { skeetkey_uses: 1 } })
-    if (res.matchedCount < 1) await CreateGuild(message)
+export const SkeetkeyUsed = async (client: Bot, message: Message): Promise<void> => {
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $inc: { skeetkey_uses: 1 } })
 }
 
-export const AddFilter = async (client: Bot, message: Message, filter: Filter) => {
-    await client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $push: { filter: filter } })
+export const AddFilter = async (client: Bot, message: Message, filter: Filter): Promise<void> => {
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $push: { filter: filter } })
 }
 
-export const RemoveFilter = async (client: Bot, message: Message, name: string) => {
-    await client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $pull: { filter: { name: name } } })
+export const RemoveFilter = async (client: Bot, message: Message, name: string): Promise<void> => {
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, { $pull: { filter: { name: name } } })
 }
 
-export const GetFilter = async (client: Bot, message: Message) => {
+export const GetFilter = async (client: Bot, message: Message): Promise<Array<Filter>> => {
     return (await client.database.database.collection("guilds").findOne({ id: message.guild.id }) as Guild).filter
 }
 
-export const GetPrefix = async (client: Bot, message: Message) => {
+export const GetPrefix = async (client: Bot, message: Message): Promise<string> => {
     return (await client.database.database.collection("guilds").findOne({ id: message.guild.id }) as Guild).prefix || "!"
 }
 
-export const SetPrefix = async (client: Bot, message: Message, prefix: string) => {
+export const SetPrefix = async (client: Bot, message: Message, prefix: string): Promise<void> => {
     client.database.database.collection("guilds").updateOne({ id: message.guild.id }, {$set: {prefix}})
+}
+
+export const GetRetardRoles = async (client: Bot, message: Message): Promise<Array<string>> => {
+    return (await client.database.database.collection("guilds").findOne({ id: message.guild.id }) as Guild).retard_roles
+}
+
+export const AddRetardRole = async (client: Bot, message: Message, id: string, position: number): Promise<void> => {
+    let query: UpdateQuery<any>
+    if (position == -1) query = {$push: {retard_roles: {$each: [id]}}}
+    else query = {$push: {retard_roles: {$each: [id], $position: position}}}
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, query)
+}
+
+export const ClearRetardRoles = async (client: Bot, message: Message): Promise<void> => {
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, {$set: {retard_roles: []}})
+}
+
+export const RemoveRetardRoleId = async (client: Bot, message: Message, id: string): Promise<void> => {
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, {$pull: {retard_roles: id}})
+}
+
+export const RemoveRetardRoleIndex = async (client: Bot, message: Message, index: number): Promise<string> => {
+    let id = (await GetRetardRoles(client, message))[index-1]
+    client.database.database.collection("guilds").updateOne({ id: message.guild.id }, {$pull: {retard_roles: id}})
+    return id
 }
