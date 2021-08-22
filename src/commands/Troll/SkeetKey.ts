2fb105d20a60cd7066db4005b70fa845a830a90d
Trollface7272 <trollface7272@post.cz>
2021-08-22 06:25:52 +0200

    Added skeetkey command

---
+++ src/commands/Troll/SkeetKey.ts
@@ -0,0 +1,21 @@
+import { Message } from "discord.js";
+import { Bot } from "../../client/Client";
+import { RunFunction } from "../../interfaces/Command";
+
+
+export const run: RunFunction = async (client: Bot, message: Message) => {
+    client.database.SkeetkeyUsed(client, message)
+    message.channel.send(`GIFT-${RandString(5)}-${RandString(5)}-${RandString(5)}`)
+}
+
+function RandString(len: number) {
+    let out = ""
+    for(;len > 0; len--) out += String.fromCharCode(Random(65,91))
+    return out
+}
+
+function Random(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+}
+
+export const name: string = "skeetkey"
