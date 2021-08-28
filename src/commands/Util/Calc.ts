fee1c23ac6b0a8c65e6a9ce80002fee17a69bdbb
Trollface7272 <trollface7272@post.cz>
2021-08-28 16:53:15 +0200

    Placeholder for future command

---
+++ src/commands/Util/Calc.ts
@@ -0,0 +1,10 @@
+import { Message } from "discord.js";
+import { Bot } from "../../client/Client";
+import { RunFunction } from "../../interfaces/Command";
+
+
+export const run: RunFunction = async (client: Bot, message: Message, args: string[]) => {
+
+}
+
+export const name: string = "gbdskfgldfgldk"
